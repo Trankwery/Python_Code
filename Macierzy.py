cf09513aa88fc0218dc0d491c1435e6f50254ed6
Trankwery <trankwery@gmail.com>
2024-04-24 19:35:57 +0200

    Dodany plik maciezy

---
+++ Macierzy.py
@@ -0,0 +1,21 @@
+Ma = [[1,2,3],[4,5,6],[7,8,9]]
+
+for m in range(3):
+    print( Ma[m] )
+
+for s in 'jakiś tekst':  
+    print(s)
+    
+for x in [0,0,0,1,2,3,4,5,6,7,8,9]:
+    print(x)
+
+for x in [0,1,2,'ok','coś',5,6,7,[34,6,78,'hklahf']]:
+    print(x)
+
+print('=========')
+for zmienna in Ma:
+    print(zmienna)
+
+print('=========')
+for zmienna in range(len(Ma)):
+    print(Ma[zmienna])
