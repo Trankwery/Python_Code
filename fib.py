85670d9d29767ecfdb03f786728e379f63a6e313
atl2137 <167915430+atl2137@users.noreply.github.com>
2024-04-26 11:38:33 +0200

    Add files via upload

--- fib.py
+++ fib.py
@@ -2,10 +2,10 @@ def fib (F):
     a=[]
     a.append(0)
     if F==0:
-        return a
+        return a[0]
     a.append(1)
     if F==1:
-        return a
+        return a[1]
     b=len(a)
     while b-1<F:
         a.append(a[b-2]+a[b-1])
@@ -17,6 +17,19 @@ def fib (F):
         
     return a[b-1]
 
+def fib_2(F):
+    a_n_1=0
+    a_n=1
+    if F==0:
+        return a_n_1
+    if F==1:
+        return a_n
+    n=1
 
-    
+    while n<F:
+        new_a_n_1=a_n
+        a_n=a_n+a_n_1
+        a_n_1=new_a_n_1
+        n+=1
 
+    return a_n
